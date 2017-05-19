57c5c42c83c796e4f7a05160668da87ead33dc7b
wei.cai <49383207@qq.com>
2017-05-19 13:40:07 +0800

    Add canvas Region Draw Path PaintFlagsDrawFilter

--- app/src/main/java/com/caiwei/customui/canvas/CanvasActivity.java
+++ app/src/main/java/com/caiwei/customui/canvas/CanvasActivity.java
@@ -7,12 +7,16 @@ import android.view.View;
 import com.caiwei.customui.R;
 
 public class CanvasActivity extends AppCompatActivity {
-    private View saveRestoreView, saveRestoreBasicView, saveRestoreLayerView;
+    private View regionClipImageView, roundImageView, canvasDrawView, saveRestoreView, saveRestoreBasicView,
+            saveRestoreLayerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_canvas);
+        regionClipImageView = findViewById(R.id.region_clip_image_view);
+        roundImageView = findViewById(R.id.round_image_view);
+        canvasDrawView = findViewById(R.id.canvas_draw_view);
         saveRestoreBasicView = findViewById(R.id.sava_restore_basic_view);
         saveRestoreView = findViewById(R.id.sava_restore_view);
         saveRestoreLayerView = findViewById(R.id.sava_restore_layer_view);
@@ -20,6 +24,9 @@ public class CanvasActivity extends AppCompatActivity {
         init(R.id.save_restore);
         init(R.id.save_restore_basic);
         init(R.id.save_restore_layer);
+        init(R.id.draw);
+        init(R.id.round);
+        init(R.id.clip);
     }
 
     private void init(int id) {
@@ -28,10 +35,22 @@ public class CanvasActivity extends AppCompatActivity {
             view.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
+                    regionClipImageView.setVisibility(View.GONE);
+                    roundImageView.setVisibility(View.GONE);
+                    canvasDrawView.setVisibility(View.GONE);
                     saveRestoreBasicView.setVisibility(View.GONE);
                     saveRestoreView.setVisibility(View.GONE);
                     saveRestoreLayerView.setVisibility(View.GONE);
                     switch (v.getId()) {
+                        case R.id.draw:
+                            canvasDrawView.setVisibility(View.VISIBLE);
+                            break;
+                        case R.id.round:
+                            roundImageView.setVisibility(View.VISIBLE);
+                            break;
+                        case R.id.clip:
+                            regionClipImageView.setVisibility(View.VISIBLE);
+                            break;
                         case R.id.save_restore_basic:
                             saveRestoreBasicView.setVisibility(View.VISIBLE);
                             break;
---
+++ app/src/main/java/com/caiwei/customui/canvas/RoundImageView.java
@@ -0,0 +1,69 @@
+package com.caiwei.customui.canvas;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PaintFlagsDrawFilter;
+import android.graphics.Path;
+import android.graphics.Rect;
+import android.graphics.Region;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.caiwei.customui.R;
+
+/**
+ * Created by John on 2017/5/17.
+ */
+
+public class RoundImageView extends View {
+    private Bitmap mBitmap;
+    private Rect mRect = new Rect();
+    private Paint mPaint = new Paint();
+    private Path mPath = new Path();
+
+    private int DEFAULT_SIZE = 100;
+
+    public RoundImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public RoundImageView(Context context) {
+        this(context, null);
+    }
+
+
+    private void init() {
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setFlags(Paint.ANTI_ALIAS_FLAG);
+        mPaint.setAntiAlias(true);// 抗锯尺
+        mBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.lsj);
+    }
+
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        setMeasuredDimension(getDefaultSize(DEFAULT_SIZE, widthMeasureSpec), getDefaultSize(DEFAULT_SIZE, heightMeasureSpec));
+    }
+
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        if (mBitmap == null) {
+            return;
+        }
+        canvas.drawColor(Color.GRAY);
+        mRect.set(0, 0, getWidth(), getHeight());
+
+        PaintFlagsDrawFilter pdf = new PaintFlagsDrawFilter(0, Paint.ANTI_ALIAS_FLAG);
+        canvas.setDrawFilter(pdf);
+
+        mPath.addCircle(getWidth() / 4, getWidth() / 4, getHeight() / 4, Path.Direction.CCW);
+        canvas.clipPath(mPath, Region.Op.REPLACE);
+
+        canvas.drawBitmap(mBitmap, null, mRect, mPaint);
+    }
+}
