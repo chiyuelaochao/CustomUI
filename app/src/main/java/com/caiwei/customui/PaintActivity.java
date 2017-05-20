5c0d0c7ff7809b6f02545a52df33659b7b4af4e5
Cai Wei <49383207@qq.com>
2017-05-20 09:11:01 +0800

    Add paint

--- app/src/main/java/com/caiwei/customui/PaintActivity.java
+++ app/src/main/java/com/caiwei/customui/PaintActivity.java
@@ -2,6 +2,9 @@ package com.caiwei.customui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.caiwei.customui.paint.CircleProgressBar;
 
 public class PaintActivity extends AppCompatActivity {
 
@@ -9,5 +12,43 @@ public class PaintActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_paint);
+
+        InitViews initViews = new InitViews();
+        initViews.initButtonAndLayout(this, R.id.paint_basic, R.id.paint_basic_view);
+        initViews.initButtonAndLayout(this, R.id.dash, R.id.dash_view);
+        initViews.initButtonAndLayout(this, R.id.dash_path, R.id.pash_path_view);
+        initViews.initButtonAndLayout(this, R.id.minion, R.id.minion_view);
+        initViews.initButtonAndLayout(this, R.id.circle_progress, R.id.circle_progress_view);
+        initViews.initButtonAndLayout(this, R.id.linear_gradient, R.id.linear_gradient_text_view);
+        initViews.initButtonAndLayout(this, R.id.gradient, R.id.gradient_view);
+        initViews.initButtonAndLayout(this, R.id.zoom, R.id.zoom_image_view);
+
+        mProgressbar = (CircleProgressBar) findViewById(R.id.circle_progress_view);
+
+        mProgressbar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        while (progress <= 100) {
+                            progress += 2;
+                            mProgressbar.setProgress(progress);
+
+                            try {
+                                Thread.sleep(100);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                    }
+                }).start();
+
+            }
+        });
     }
+
+    private CircleProgressBar mProgressbar;
+    private int progress = 0;
 }
