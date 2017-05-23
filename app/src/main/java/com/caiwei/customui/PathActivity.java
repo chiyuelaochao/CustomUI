df667950e4e479727c9292ca42983d6ad1420965
wei.cai <49383207@qq.com>
2017-05-23 12:23:02 +0800

    Add DragBubbleView

--- app/src/main/java/com/caiwei/customui/PathActivity.java
+++ app/src/main/java/com/caiwei/customui/PathActivity.java
@@ -1,7 +1,11 @@
 package com.caiwei.customui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.caiwei.customui.path.DragBubbleView;
 
 public class PathActivity extends AppCompatActivity {
 
@@ -13,5 +17,23 @@ public class PathActivity extends AppCompatActivity {
         initViews.initButtonAndLayout(this, R.id.curve, R.id.curve_view);
         initViews.initButtonAndLayout(this, R.id.path_fill, R.id.path_fill_view);
         initViews.initButtonAndLayout(this, R.id.path_op, R.id.path_op_view);
+        initViews.initButtonAndLayout(this, R.id.drag_bubble, R.id.drag_bubble_layout);
+
+        initDragBubbleView();
+    }
+
+    private void initDragBubbleView() {
+        final DragBubbleView view = (DragBubbleView) findViewById(R.id.drag_bubble_view);
+        Button button = (Button) findViewById(R.id.reset_btn);
+        if (button != null) {
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (view != null) {
+                        view.reset();
+                    }
+                }
+            });
+        }
     }
 }
